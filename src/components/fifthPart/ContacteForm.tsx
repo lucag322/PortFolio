30edb0ff112776f529d5728223f488972230bb06
lucag322 <97728486+lucag322@users.noreply.github.com>
2022-09-11 21:14:16 +0200

    re test

--- src/components/fifthPart/ContacteForm.tsx
+++ src/components/fifthPart/ContacteForm.tsx
@@ -1,23 +1,74 @@
 import React from 'react';
 import "./../../css/fifthPart.css";
+import {useState} from 'react';
+
+interface FormPost {
+  firstname?: string
+  lastname?: string
+  email?: string
+}
 
 
-   function ContactForm ( ) {
-      return (
-        <div>
-            <form name='contact' method='POST' className='contact_form' data-netilify='true'>
-                <label htmlFor="name">Nom: </label>
-                <input type="text" name='name'/>
-
-                <label htmlFor="email">Email: </label>
-                <input type="text" name='email'/>
-
-                <label htmlFor="message">Message: </label>
-                <textarea name='message'/>
-                
-                <button type="submit">Send</button>
-            </form>
-       </div>
-       )
-       }
-    export default ContactForm
+const Form = () => {
+    const encode = (data: any) => {
+      return Object.keys(data)
+          .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+          .join("&");
+    };
+  
+    const [state, setState] = useState<FormPost>();
+    const [submitted, setSubmitted] = useState(false);
+  
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", ...state })
+      })
+        .then(() => console.log("Success!"))
+        .catch(error => console.log(error));
+  
+      event.preventDefault();
+      setSubmitted(true);
+    }
+  
+    const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+      setState({
+        ...state,
+        [e.currentTarget.id]: e.currentTarget.value,
+      });
+    };
+  
+    return (
+       <div>
+          { (!submitted) && <form name="contact" method="POST" data-netlify="true" onSubmit={onSubmit}>
+          <input type="hidden" name="form-name" value="contact" />
+          <div>
+            <div>
+            <input type="text" id="firstname" name="firstname" placeholder="First Name"
+            onChange={handleChange} />
+            </div>
+            <div>
+            <input type="text" id="lastname" name="lastname" placeholder="Last Name"
+            onChange={handleChange} />
+            </div>
+          </div>
+          <div>
+              <input type="text" name="email" id="email" placeholder="Email"
+              onChange={handleChange} />
+          </div>
+          <div>
+            <button type="submit">
+                Get Access
+            </button>
+          </div>
+        </form>}
+        { submitted && 
+          <h5>Thanks for submitting! We&apos;ll reach out ASAP!</h5>
+        }
+      </div>
+    )
+  
+  }
+    export default Form
