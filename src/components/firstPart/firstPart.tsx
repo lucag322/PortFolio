5ede5dcde07aede3ebce53913e348a6a4ecc7417
lucag322 <97728486+lucag322@users.noreply.github.com>
2022-09-20 22:03:15 +0200

    fiouuuu

--- src/components/firstPart/firstPart.tsx
+++ src/components/firstPart/firstPart.tsx
@@ -3,9 +3,50 @@ import "./../../css/firstPart.css";
 import { Container, Row, Col } from "react-bootstrap";
 import { useAnimation, motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+
+
+
+
+
+
 import Hours from "./Hours";
 
 function FirstPart() {
+
+  const changContentContact = () =>{
+   document.querySelector("#Contact")!.innerHTML ='Contact';
+    };
+  
+  const resetContentContact = () =>{
+   document.querySelector("#Contact")!.innerHTML = ' <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" class="svg-inline--fa fa-circle dote" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512z"></path></svg> ';
+  }
+
+  const changContentAbout = () =>{
+    document.querySelector("#About")!.innerHTML ='About';
+     };
+   
+   const resetContentAbout = () =>{
+    document.querySelector("#About")!.innerHTML = ' <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" class="svg-inline--fa fa-circle dote" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512z"></path></svg> ';
+   }
+
+   const changContentProject = () =>{
+    document.querySelector("#Project")!.innerHTML ='Project';
+     };
+   
+   const resetContentProject = () =>{
+    document.querySelector("#Project")!.innerHTML = ' <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" class="svg-inline--fa fa-circle dote" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512z"></path></svg> ';
+   }
+
+   const changContentTop = () =>{
+    document.querySelector("#Top")!.innerHTML ='Top';
+     };
+   
+   const resetContentTop = () =>{
+    document.querySelector("#Top")!.innerHTML = ' <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" class="svg-inline--fa fa-circle dote" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512z"></path></svg> ';
+   }
+
 
   const scrollToTop = () => {
     window.scrollTo({
@@ -16,7 +57,6 @@ function FirstPart() {
 
   return (
     <div className="firstPart">
-      <div className="gradiant">
         <Container>
           <Row className="firstRow">
             <Col>
@@ -34,20 +74,27 @@ function FirstPart() {
         </Container>
         <nav className="noSelect navbarCustom">
           <ul className="navbarContent thrn fw-bold ">
-            <li onClick={scrollToTop}> Skills</li>
-            <li className="py-2">-</li>
-            <li>About </li>
-            <li className="py-2">-</li>
-            <li>Work </li>
-            <li className="py-2">-</li>
-            <li>Contact</li>
+          <li className="py-2"><div className="bigTiret"/></li>
+          
+            <li onMouseOut={resetContentContact} onMouseOver={changContentContact} onClick={scrollToTop} id="Contact" className="Navitemrota">
+              <FontAwesomeIcon icon={ faCircle } className="dote"/>
+            </li>
+            <li className="py-2"><div className="tiret"/></li>
+            <li onMouseOut={resetContentAbout} onMouseOver={changContentAbout} id="About" className="Navitemrota">
+              <FontAwesomeIcon icon={ faCircle }className="dote" /> </li>
+            <li className="py-2"><div className="tiret"/></li>
+            <li onMouseOut={resetContentProject} onMouseOver={changContentProject} id="Project" className="Navitemrota">
+              <FontAwesomeIcon icon={ faCircle }className="dote" /> </li>
+            <li className="py-2"><div className="tiret"/></li>
+            <li onMouseOut={resetContentTop} onMouseOver={changContentTop} id="Top" className="Navitemrota">
+              <FontAwesomeIcon icon={ faCircle }className="dote" /> </li>
+          <li className="py-2"><div className="bigTiret"/></li>
           </ul>
         </nav>
         <span>
           <Hours/>
         </span>
       </div>
-    </div>
   );
 }
 
