4be7d3d5e79216694b85ac1a84fb761b45a9f43c
lucalg322 <95612509+lucalg322@users.noreply.github.com>
2022-10-05 11:11:33 +0200

    add project block de merde

--- src/components/secondPart/secondPart.tsx
+++ src/components/secondPart/secondPart.tsx
@@ -5,9 +5,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import { useAnimation, motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
-
 function secondPart() {
-  
   const { ref, inView } = useInView();
   const animation1 = useAnimation();
   const animation2 = useAnimation();
@@ -37,16 +35,22 @@ function secondPart() {
     }
   }, [inView]);
 
-
   return (
     <div className="secondPart skillsTitle pb-5">
-      <Container >
+      <Container>
         <Row className="pb-5" ref={ref}>
           <Col lg={9} sm={9} xs={9}>
-
-
-            <motion.h2 animate={animation1} className="partName  fw-light fst-italic pt-3 position-relative zindex-sticky"> Skills</motion.h2>
-            <motion.p animate={animation2} className="yrsa position-relative zindex-sticky">
+            <motion.h2
+              animate={animation1}
+              className="partName  fw-light fst-italic pt-3 position-relative zindex-sticky"
+            >
+              {" "}
+              Skills
+            </motion.h2>
+            <motion.p
+              animate={animation2}
+              className="yrsa position-relative zindex-sticky"
+            >
               Lorem ipsum dolor sit amet.Sed beatae quas sit fugit fugiat sit
               placeat doloribus ut eius atque est odio voluptates. Ut possimus
               veritatis eum libero deleniti ab illo optio Lorem ipsum dolor sit
@@ -55,12 +59,15 @@ function secondPart() {
               deleniti ab illo optio
             </motion.p>
           </Col>
-          <Col lg={3} sm={3} xs={3}>
-           
-          </Col>
+          <Col lg={3} sm={3} xs={3}></Col>
         </Row>
-        <Row className="d-flex justify-content-between position-relative zindex " id="drag">
-          <Slider/>
+      </Container>
+      <Container className="carrousel">
+        <Row
+          className="d-flex justify-content-between position-relative zindex "
+          id="drag"
+        >
+          <Slider />
         </Row>
       </Container>
     </div>
--- src/components/thirdPart/thirdPart.tsx
+++ src/components/thirdPart/thirdPart.tsx
@@ -4,70 +4,76 @@ import Img1 from "./../../img/capture1.png";
 import Img2 from "./../../img/capture2.png";
 import Img3 from "./../../img/capture3.png";
 import Img4 from "./../../img/capture4.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight, faCircle } from '@fortawesome/free-solid-svg-icons';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faCircle } from "@fortawesome/free-solid-svg-icons";
 
 function thirdPart() {
   const ref: any = useRef();
 
-  return(
-   <div className="thirdPart" id="drag"> 
-   
-
-        <div className="card yrsa first">
-          <div className="img-box">
-            <img src={Img1} className="card-img-top" alt="..."/>
-          </div>
+  return (
+    <div className="thirdPart" id="drag">
+      <div className="card yrsa first">
+        <div className="img-box">
+          <img src={Img1} className="card-img-top" alt="..." />
+        </div>
         <div className="card-body">
-          <h3 className="card-title">Card test</h3>
-          <a href="#" id="btn" className="btn btn-primary">Voir plus <FontAwesomeIcon icon={ faArrowRight }className="btn-arrow" /> </a>
+          <h3 className="card-title">Card title</h3>
+          <a href="#" className="btn btn-primary">
+            Voir plus{" "}
+            <FontAwesomeIcon icon={faArrowRight} className="btn-arrow" />
+          </a>
         </div>
       </div>
       <div className="card yrsa">
         <div className="img-box">
-           <img src={Img2} className="card-img-top" alt="..."/>
+          <img src={Img2} className="card-img-top" alt="..." />
         </div>
         <div className="card-body">
           <h3 className="card-title">Card title</h3>
-          <a href="#" className="btn btn-primary">Voir plus <FontAwesomeIcon icon={ faArrowRight }className="btn-arrow" /></a>
+          <a href="#" className="btn btn-primary">
+            Voir plus{" "}
+            <FontAwesomeIcon icon={faArrowRight} className="btn-arrow" />
+          </a>
         </div>
       </div>
       <div className="card yrsa">
         <div className="img-box">
-            <img src={Img3} className="card-img-top" alt="..."/>
+          <img src={Img3} className="card-img-top" alt="..." />
         </div>
         <div className="card-body">
           <h3 className="card-title">Card title</h3>
-          <a href="#" className="btn btn-primary">Voir plus <FontAwesomeIcon icon={ faArrowRight }className="btn-arrow" /></a>
+          <a href="#" className="btn btn-primary">
+            Voir plus{" "}
+            <FontAwesomeIcon icon={faArrowRight} className="btn-arrow" />
+          </a>
         </div>
       </div>
       <div className="card yrsa">
         <div className="img-box">
-          <img src={Img4} className="card-img-top" alt="..."/>
+          <img src={Img4} className="card-img-top" alt="..." />
         </div>
         <div className="card-body">
           <h3 className="card-title">Card title</h3>
-          <a href="#" className="btn btn-primary">Voir plus <FontAwesomeIcon icon={ faArrowRight }className="btn-arrow" /></a>
+          <a href="#" className="btn btn-primary">
+            Voir plus{" "}
+            <FontAwesomeIcon icon={faArrowRight} className="btn-arrow" />
+          </a>
         </div>
       </div>
       <div className="card yrsa">
         <div className="img-box">
-          <img src={Img2} className="card-img-top" alt="..."/>
+          <img src={Img2} className="card-img-top" alt="..." />
         </div>
         <div className="card-body">
           <h3 className="card-title">Card title</h3>
-          <a href="#" className="btn btn-primary">Voir plus <FontAwesomeIcon icon={ faArrowRight }className="btn-arrow" /></a>
+          <a href="#" className="btn btn-primary">
+            Voir plus{" "}
+            <FontAwesomeIcon icon={faArrowRight} className="btn-arrow" />
+          </a>
         </div>
       </div>
-      </div>
-    
-
-    
-          
-        
-
-  )
+    </div>
+  );
 }
 
 export default thirdPart;
