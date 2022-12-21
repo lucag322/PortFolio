f2fa3f3642868fd6c0dbe1858a944b222265a160
Luca Grousset <lg@lamp-ic.fr>
2022-12-21 17:24:09 +0100

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
- const defineConfig = ({
-  build: {
-    rollupOptions: {
-      // https://rollupjs.org/guide/en/#big-list-of-options
-    }
-  }
-});
-export default defineConfig;
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()] 
+})
