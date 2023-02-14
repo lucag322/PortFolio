4d4692f364ba0445daeefa99cb938a0f63a11f3e
lucalg322 <95612509+lucalg322@users.noreply.github.com>
2023-02-14 18:50:58 +0100

    add

--- portfolioFront/src/components/home/fourthPart/fourthPart.tsx
+++ portfolioFront/src/components/home/fourthPart/fourthPart.tsx
@@ -32,10 +32,10 @@ function fourthPart() {
 
   const cardVariants: Variants = {
     offscreen: {
-      y: 300,
+      y: 100,
     },
     onscreen: {
-      y: 50,
+      y: 0,
       transition: {
         type: "spring",
         bounce: 0.4,
@@ -51,26 +51,19 @@ function fourthPart() {
           initial="offscreen"
           whileInView="onscreen"
           viewport={{ once: true, amount: 0.8 }}
-          className="buble3 row"
-        >
-          <motion.h2
-            variants={cardVariants}
-            className="partName  fw-light fst-italic pt-2 position-relative text-end"
-            id="Titre"
-          >
-            about me
-          </motion.h2>
-        </motion.div>
-        <motion.div
-          initial="offscreen"
-          whileInView="onscreen"
-          viewport={{ once: true, amount: 0.8 }}
           className="row"
         >
           <Col lg={4} sm={4} xs={4} className=" p-0">
             <img src={star} alt="" className="rrr" />
           </Col>
-          <Col lg={8} sm={8} xs={8} className="d-flex align-items-center">
+          <Col lg={8} sm={8} xs={8} className="">
+            <motion.h2
+              variants={cardVariants}
+              className="partName  fw-light fst-italic pt-2 position-relative text-end"
+              id="Titre"
+            >
+              about me
+            </motion.h2>
             {isLoading
               ? "loading"
               : homes.map((item: any) => (
